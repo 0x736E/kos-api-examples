98e7305b6c18a3f7776bd88ac2d7eeaea3f48c96
seannicholls <seannicholls@gmail.com>
2016-01-02 03:02:54 +0000

    full name instead of github username

--- MyKspMod/Properties/AssemblyInfo.cs
+++ MyKspMod/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("seannicholls")]
+[assembly: AssemblyCopyright ("Sean Nicholls")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
